7190684a6e51a440df5321ea7ab31da96a2145a9
SURYA IDURI <tejs4100gmail.com>
2022-11-02 12:49:45 +0530

    first commit

---
+++ tag.js
@@ -0,0 +1,6 @@
+var option = document.getElementsByClassName('list-group-option');
+console.log(option);
+console.log(option[1]);
+option[1].textContent = 'hi';
+option[1].style.fontweight='bold';
+option[1].style.backgroundcolor = 'red'
